0d345c9e35aa87b9966a31623a7b80b6c12a6086
Richard Djanie Amasah <djanie.amasah@gmail.com>
2025-09-19 02:42:04 +0000

    add dag task

---
+++ dags/data_pipeline.py
@@ -0,0 +1,65 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import sys
+import os
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+}
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.generator.generate_data import generate_orders_csv
+from src.processor.validate import validate_orders
+from src.processor.transform import transform_orders
+from src.db.upsert import upsert_orders
+
+
+def generate():
+    generate_orders_csv()
+
+def validate():
+    valid, err = validate_orders('data/incoming/orders.csv')
+    if not valid:
+        raise ValueError(f"Validation failed: {err}")
+
+def transform():
+    transform_orders()
+
+def upsert():
+    upsert_orders('data/processed/orders_transformed.csv')
+
+
+with DAG('data_pipeline',
+         default_args=default_args,
+         schedule_interval='@daily',
+         max_active_runs=1,
+         catchup=False) as dag:
+
+    generate_task = PythonOperator(
+        task_id='generate_data',
+        python_callable=generate
+    )
+
+    validate_task = PythonOperator(
+        task_id='validate_data',
+        python_callable=validate
+    )
+
+    transform_task = PythonOperator(
+        task_id='transform_data',
+        python_callable=transform
+    )
+
+    upsert_task = PythonOperator(
+        task_id='upsert_data',
+        python_callable=upsert
+    )
+
+    generate_task >> validate_task >> transform_task >> upsert_task
