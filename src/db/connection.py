292d63fb31762f936464702810c29bcabcd1cbb6
Richard Djanie Amasah <djanie.amasah@gmail.com>
2025-09-19 02:17:43 +0000

    add db connection details

---
+++ src/db/connection.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+import os
+
+def get_db_engine():
+    user = os.getenv('POSTGRES_USER', 'postgres')
+    password = os.getenv('POSTGRES_PASSWORD', 'postgres')
+    host = os.getenv('POSTGRES_HOST', 'postgres')  # inside docker network
+    port = os.getenv('POSTGRES_PORT', '5432')
+    dbname = os.getenv('POSTGRES_DB', 'orders_db')
+
+    db_url = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}'
+    engine = create_engine(db_url)
+    return engine
+
+if __name__ == "__main__":
+    engine = get_db_engine()
+    with engine.connect() as connection:
+        result = connection.execute("SELECT version();")
+        version = result.fetchone()
+        print(f"Connected to PostgreSQL version: {version[0]}")
