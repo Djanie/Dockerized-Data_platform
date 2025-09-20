9a4c76ca9d5c0c8faff31705633f71af855321fd
Richard Djanie Amasah <djanie.amasah@gmail.com>
2025-09-20 09:21:02 +0000

    feat: add setup guide in root

---
+++ setup.py
@@ -0,0 +1,9 @@
+# setup.py (place at repo root)
+from setuptools import setup, find_packages
+
+setup(
+    name="data_pipeline_src",
+    version="0.0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
