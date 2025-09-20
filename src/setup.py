3c6cc52090ffbe94d01b0e165d683dbb206a99c9
Richard Djanie Amasah <djanie.amasah@gmail.com>
2025-09-20 09:34:12 +0000

    feat:add setup guide in src root

--- src/setup.py
+++ src/setup.py
@@ -3,6 +3,6 @@
 setup(
     name="data_pipeline_src",
     version="0.0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where="."),
+    package_dir={"": "."},
 )
