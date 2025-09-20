a21acf603b00e5790b701a14c8d0dbfbb370c6c7
Richard Djanie Amasah <djanie.amasah@gmail.com>
2025-09-20 14:25:31 +0000

    fix: change db connection details

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine, text
 import os
 
-def get_db_engine():
-    user = os.getenv('POSTGRES_USER', 'postgres')
-    password = os.getenv('POSTGRES_PASSWORD', 'postgres')
-    host = os.getenv('POSTGRES_HOST', 'localhost')   # or 'postgres' if inside docker
-    port = os.getenv('POSTGRES_PORT', '5433')
-    dbname = os.getenv('POSTGRES_DB', 'orders_db')
 
-    db_url = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}'
-    engine = create_engine(db_url)
-    return engine
+
+def get_db_engine():
+    user = os.getenv("POSTGRES_USER", "postgres")
+    password = os.getenv("POSTGRES_PASSWORD", "postgres")
+    db = os.getenv("POSTGRES_DB", "orders_db")
+    host = os.getenv("POSTGRES_HOST", "postgres")
+    port = os.getenv("POSTGRES_PORT", 5432)
+    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
+    return create_engine(url)
 
 if __name__ == "__main__":
     engine = get_db_engine()
